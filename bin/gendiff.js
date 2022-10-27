065d70e15daa14ab0f019ca351f7318a06689eb7
Elena Ozerova <ozerova.lenka@gmail.com>
2022-10-27 20:36:43 +0200

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 import { Command } from "commander";
+
 const program = new Command();
 
 program
   .name("gendiff")
   .description("Compares two configuration files and shows a difference.")
   .version("0.0.1", "-V, --version", "output the version number")
-  .helpOption("-h, --help", "display help for command");
+  .helpOption("-h, --help", "display help for command")
+  .option("-f, --format <type>", "output format")
+  .arguments("<filepath1> <filepath2>");
 
 program.parse(process.argv);
-
-const options = program.opts();
-if (options.version) console.log(`${options.version}`);
-if (options.helpOption) console.log(`${options.helpOption}`);
-
 
 console.log("DONE!");
