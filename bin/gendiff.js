7899d151e7f0fe2c666c6e98d4486fa1a1ae8268
Elena Ozerova <ozerova.lenka@gmail.com>
2022-10-27 17:02:59 +0200

    step 1 complete

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,22 +5,14 @@ const program = new Command();
 program
   .name("gendiff")
   .description("Compares two configuration files and shows a difference.")
-  .version("0.0.1")
-  .option("-V, --version", "output the version number 1")
-  .option("-h, --help", "display help for command 1");
+  .version("0.0.1", "-V, --version", "output the version number")
+  .helpOption("-h, --help", "display help for command");
 
 program.parse(process.argv);
 
 const options = program.opts();
 if (options.version) console.log(`${options.version}`);
-if (options.help) console.log(`${options.help}`);
+if (options.helpOption) console.log(`${options.helpOption}`);
 
-// program.command('gendiff')
-// .action((str, options) => {
-//   const limit = options.help ? 1 : undefined;
-//   console.log(str.split(options.version, limit));
-// });
-
-// program.parse();
 
 console.log("DONE!");
