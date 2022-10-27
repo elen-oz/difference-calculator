57f220ae5f49e264d5127f958bcf6756606aa933
Elena Ozerova <ozerova.lenka@gmail.com>
2022-10-27 14:45:56 +0200

    1 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,25 @@ program
   .name("gendiff")
   .description("Compares two configuration files and shows a difference.")
   .version("0.0.1")
-  .option("-V, --version", "output the version number")
-  .option("-h, --help", "display help for command");
+  .option("-V, --version", "output the version number 1")
+  .option("-h, --help", "display help for command 1");
+
+// program.parse(process.argv);
+
+// const options = program.opts();
+// if (options.version) console.log(`${options.version}`);
+// if (options.help) console.log(`${options.help}`);
+
+
+
+program.command('gendiff')
+// .option("-V, --version", "output the version number")
+// .option("-h, --help", "display help for command")
+.action((str, options) => {
+  const limit = options.help ? 1 : undefined;
+  console.log(str.split(options.version, limit));
+});
 
 program.parse();
 
-console.log('I am working!' );
+console.log("DONE!");
