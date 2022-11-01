c17bac606302fc977bf6d676ac3ab1e21ffd9310
Elena Ozerova <ozerova.lenka@gmail.com>
2022-11-01 10:37:07 +0100

    step 2

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { readFileSync } from "node:fs";
 
-const fileData1 = readFileSync("./data/file1.json");
-console.log(fileData1.toString());
-const fileData2 = readFileSync("./data/file2.json");
-console.log(fileData2.toString());
+const fileData1 = readFileSync("./data/file1.json", "utf8");
+console.log(fileData1);
+const fileData2 = readFileSync("./data/file2.json", "utf8");
+console.log(fileData2);
