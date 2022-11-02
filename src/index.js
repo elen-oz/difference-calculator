7400fe4344adb45628b193f28ffeb6a04300cd58
Elena Ozerova <ozerova.lenka@gmail.com>
2022-11-02 15:29:01 +0100

    step 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 #!/usr/bin/env node
 import { readFileSync } from "node:fs";
+import path from 'path';
+import { cwd } from 'process';
 
 export default (filepath1, filepath2) => {
-  const fileData1 = readFileSync("./data/file1.json", "utf8");
-  console.log(fileData1);
-  const fileData2 = readFileSync("./data/file2.json", "utf8");
-  console.log(fileData2);
+  filepath1 = path.resolve(cwd(), filepath1).trim();
+  filepath2 = path.resolve(cwd(), filepath2).trim();
 
-  console.log(typeof(fileData1));
-  console.log(typeof(fileData2));
 
+  console.log("filepath1 ", filepath1);
+  console.log("filepath2 ", filepath2);
+
+  // console.log(`Current directory: ${cwd()}`);
+
+
+  const fileData1 = readFileSync(filepath1, "utf8");
+  const fileData2 = readFileSync(filepath2, "utf8");
+
+  const dataParse1 = JSON.parse(fileData1);
+  const dataParse2 = JSON.parse(fileData2);
+
+  // console.log("typeof(fileData1)", typeof fileData1);
+  // console.log("typeof(fileData2)", typeof fileData2);
+
+  // console.log("dataParse1", dataParse1);
+  // console.log("dataParse2", dataParse2);
 };
