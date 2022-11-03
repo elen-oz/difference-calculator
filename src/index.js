2227686a1112c7f9b7f65acb47d5b2d02103ae96
Elena Ozerova <ozerova.lenka@gmail.com>
2022-11-03 21:17:13 +0100

    step2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
 #!/usr/bin/env node
+/* eslint-disable no-restricted-syntax */
 import { readFileSync } from "node:fs";
-import path from 'path';
-import { cwd } from 'process';
+import path from "path";
+import { cwd } from "process";
+import _ from "lodash";
 
 export default (filepath1, filepath2) => {
   filepath1 = path.resolve(cwd(), filepath1).trim();
   filepath2 = path.resolve(cwd(), filepath2).trim();
 
-
-  console.log("filepath1 ", filepath1);
-  console.log("filepath2 ", filepath2);
-
-  // console.log(`Current directory: ${cwd()}`);
-
-
   const fileData1 = readFileSync(filepath1, "utf8");
   const fileData2 = readFileSync(filepath2, "utf8");
 
-  const dataParse1 = JSON.parse(fileData1);
-  const dataParse2 = JSON.parse(fileData2);
+  const data1 = JSON.parse(fileData1);
+  const data2 = JSON.parse(fileData2);
 
-  // console.log("typeof(fileData1)", typeof fileData1);
-  // console.log("typeof(fileData2)", typeof fileData2);
+  const keys1 = _.keys(data1);
+  const keys2 = _.keys(data2);
 
-  // console.log("dataParse1", dataParse1);
-  // console.log("dataParse2", dataParse2);
+  const keys = _.sortBy(_.union(keys1, keys2));
+  // console.log("keys: ", keys);
+
+  const getInfoDiff = (data1, data2, keys) => {
+    const result = {};
+    for (const key of keys) {
+      if (!_.has(data1, key)) {
+        result[key] = "added";
+      } else if (!_.has(data2, key)) {
+        result[key] = "deleted";
+      } else if (data1[key] !== data2[key]) {
+        result[key] = "changed";
+      } else {
+        result[key] = "unchanged";
+      }
+    }
+
+    return result;
+  };
+
+  console.log(getInfoDiff(data1, data2, keys));
 };
