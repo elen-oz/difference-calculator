a435fb12164cdd536fb99ffe36203e9244ccf595
Elena Ozerova <ozerova.lenka@gmail.com>
2022-11-22 22:23:17 +0100

    stage 4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-underscore-dangle */
 import { test, expect } from '@jest/globals';
-// import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
@@ -18,17 +17,4 @@ const expectedResult = readFile('outputtest.txt', 'utf8').trim();
 
 test('check genDiff', () => {
   expect(genDiff(file1, file2)).toEqual(expectedResult);
-  // expect(originResult).toEqual(expectedResult);
 });
-
-// const filepath1 = '../__fixtures__/testFile1.json';
-// const filepath2 = '../__fixtures__/testFile2.json';
-
-// const fileFullPath1 = path.resolve(cwd(), filepath1).trim();
-// const fileFullPath2 = path.resolve(cwd(), filepath2).trim();
-
-// const fileData1 = readFileSync(fileFullPath1, 'utf8');
-// const fileData2 = readFileSync(fileFullPath2, 'utf8');
-
-// const data1 = JSON.parse(fileData1);
-// const data2 = JSON.parse(fileData2);
